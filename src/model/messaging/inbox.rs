842047c8117b38ea10e1a95d4ccccc26c0461808
ion232 <66679118+ion232@users.noreply.github.com>
2023-02-17 14:58:17 +0000

    format: model/messaging

--- src/model/messaging/inbox.rs
+++ src/model/messaging/inbox.rs
@@ -1,24 +1,26 @@
+use crate::model::{Body, Request};
 use serde::Serialize;
-use crate::model::{Request, Body};
 
 #[derive(Default, Clone, Debug, Serialize)]
-pub struct Query;
+pub struct PostQuery;
 
 #[derive(Default, Clone, Debug, Serialize)]
 pub struct Message {
-    text: String
+    text: String,
 }
 
-pub type PostRequest = Request<Query, Message>;
+pub type PostRequest = Request<PostQuery, Message>;
 
 impl PostRequest {
     pub fn new(username: &str, message: &str) -> Self {
-        let message = Message{text: message.to_string()};
+        let message = Message {
+            text: message.to_string(),
+        };
         Self {
             method: http::Method::POST,
             path: format!("/inbox/{}", username).to_string(),
             query: Default::default(),
-            body: Body::Form(message)
+            body: Body::Form(message),
         }
     }
 }
