62b0f3b72b60e49870f77a414aa989fd81771616
Arran Ireland <66679118+ion232@users.noreply.github.com>
2024-01-20 12:16:54 +0000

    feat: add lichess endpoints for openings

--- src/api/opening.rs
+++ src/api/opening.rs
@@ -2,10 +2,12 @@ use crate::client::LichessApi;
 use crate::error::Result;
 use crate::model::opening::*;
 
-use self::masters::OpeningExplorerJson;
-
 impl LichessApi<reqwest::Client> {
-    pub async fn masters_openings(&self, request: masters::GetRequest) -> Result<OpeningExplorerJson> {
+    pub async fn openings_masters(&self, request: masters::GetRequest) -> Result<OpeningExplorerJson> {
+        self.get_single_model(request).await
+    }
+
+    pub async fn openings_lichess(&self, request: lichess::GetRequest) -> Result<OpeningExplorerJson> {
         self.get_single_model(request).await
     }
 }
--- src/model/opening/mod.rs
+++ src/model/opening/mod.rs
@@ -1,1 +1,66 @@
+pub mod lichess;
 pub mod masters;
+
+use serde::{Deserialize, Serialize};
+
+use crate::model::Color;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde_with::skip_serializing_none]
+pub struct OpeningExplorerJson {
+    pub opening: Option<Opening>,
+    pub white: u32,
+    pub draws: u32,
+    pub black: u32,
+    pub moves: Vec<Move>,
+    pub top_games: Vec<Game>,
+    pub recent_games: Option<Vec<Game>>,
+    pub history: Option<Vec<History>>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Opening {
+    pub eco: String,
+    pub name: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde_with::skip_serializing_none]
+pub struct Move {
+    pub uci: String,
+    pub san: String,
+    pub average_rating: Option<u32>,
+    pub white: u32,
+    pub draws: u32,
+    pub black: u32,
+    pub game: Option<Game>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde_with::skip_serializing_none]
+pub struct Game {
+    pub id: String,
+    pub winner: Option<Color>,
+    pub white: Player,
+    pub black: Player,
+    pub year: u32,
+    pub month: String,
+    pub uci: Option<String>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Player {
+    pub name: String,
+    pub rating: u32,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct History {
+    pub month: String,
+    pub black: u32,
+    pub draws: u32,
+    pub white: u32,
+}
+
